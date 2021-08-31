d34d7a30f8a26fe1c3b7f218632d9df1f7a005fe
Al Ganiev <helios.ag@gmail.com>
2021-08-31 23:50:48 +1000

    Added typed props to preexec event

--- src/Event/ElFinderPreExecutionEvent.php
+++ src/Event/ElFinderPreExecutionEvent.php
@@ -1,8 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace FM\ElfinderBundle\Event;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Contracts\EventDispatcher\Event;
 
@@ -10,39 +13,25 @@ class ElFinderPreExecutionEvent extends Event
 {
     /**
      * Request object containing ElFinder command and parameters.
-     *
-     * @var Request
      */
-    protected $request;
+    protected Request $request;
 
     /**
      * Used to make sub requests.
-     *
-     * @var HttpKernelInterface
      */
-    private $httpKernel;
+    private HttpKernelInterface $httpKernel;
 
     /**
      * ElFinder instance.
-     *
-     * @var string
      */
-    protected $instance;
+    protected string $instance;
 
     /**
      * Home folder.
-     *
-     * @var string
      */
-    protected $homeFolder;
+    protected string $homeFolder;
 
-    /**
-     * Constructor.
-     *
-     * @param string $instance
-     * @param string $homeFolder
-     */
-    public function __construct(Request $request, HttpKernelInterface $httpKernel, $instance, $homeFolder)
+    public function __construct(Request $request, HttpKernelInterface $httpKernel, string $instance, string $homeFolder)
     {
         $this->request    = $request;
         $this->httpKernel = $httpKernel;
@@ -50,18 +39,7 @@ class ElFinderPreExecutionEvent extends Event
         $this->homeFolder = $homeFolder;
     }
 
-    /**
-     * Makes a sub request to elFinder.
-     * Function based on 'forward' function from Symfony controllers.
-     *
-     * @see https://github.com/symfony/symfony/blob/2.5/src/Symfony/Bundle/FrameworkBundle/Controller/Controller.php
-     *
-     * @param array $path  An array of path parameters
-     * @param array $query An array of query parameters
-     *
-     * @return Symfony\Component\HttpFoundation\Response A Response instance
-     */
-    public function subRequest(array $path, array $query)
+    public function subRequest(array $path, array $query): Response
     {
         $path['_controller'] = 'FMElfinderBundle:ElFinder:load';
         $subRequest          = $this->request->duplicate($query, null, $path);
@@ -71,34 +49,23 @@ class ElFinderPreExecutionEvent extends Event
 
     /**
      * Returns executed command.
-     *
-     * @return string
      */
-    public function getCommand()
+    public function getCommand(): string
     {
         return $this->request->get('cmd');
     }
 
-    /**
-     * @return Request
-     */
-    public function getRequest()
+    public function getRequest(): Request
     {
         return $this->request;
     }
 
-    /**
-     * @return string
-     */
-    public function getInstance()
+    public function getInstance(): string
     {
         return $this->instance;
     }
 
-    /**
-     * @return string
-     */
-    public function getHomeFolder()
+    public function getHomeFolder(): string
     {
         return $this->homeFolder;
     }
