b98e7c1c39e4e6465d358ce7f3c8c4f4b3bcd29d
Al Ganiev <helios.ag@gmail.com>
2021-09-01 00:21:43 +1000

    Added typed props to post exec event

--- src/Event/ElFinderPostExecutionEvent.php
+++ src/Event/ElFinderPostExecutionEvent.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace FM\ElfinderBundle\Event;
 
 use Symfony\Component\HttpFoundation\Request;
@@ -9,18 +11,10 @@ class ElFinderPostExecutionEvent extends ElFinderPreExecutionEvent
 {
     /**
      * Command execution result.
-     *
-     * @var array
      */
-    protected $result;
+    protected array $result;
 
-    /**
-     * Constructor.
-     *
-     * @param string $instance
-     * @param string $homeFolder
-     */
-    public function __construct(Request $request, HttpKernelInterface $httpKernel, $instance, $homeFolder, array $result)
+    public function __construct(Request $request, HttpKernelInterface $httpKernel, string $instance, string $homeFolder, array $result)
     {
         parent::__construct($request, $httpKernel, $instance, $homeFolder);
 
@@ -29,23 +23,18 @@ class ElFinderPostExecutionEvent extends ElFinderPreExecutionEvent
 
     /**
      * Tells if execution has encountered errors.
-     *
-     * @return bool
      */
-    public function hasErrors()
+    public function hasErrors(): bool
     {
         return isset($this->result['error']);
     }
 
-    /**
-     * @return array
-     */
-    public function getResult()
+    public function getResult(): array
     {
         return $this->result;
     }
 
-    public function setResult(array $result)
+    public function setResult(array $result): void
     {
         $this->result = $result;
     }
